bd5149be211a9d3120ce941b5229952653c916a4
Rahul <rahul.shial@gmail.com>
2020-10-26 22:40:09 +0000

    Assert Equal ver 0.001

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(":) Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(":( Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Apple", "Apple");
