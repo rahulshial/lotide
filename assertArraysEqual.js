27f3bd8879a318efa221368e459814d439c1352e
Rahul <rahul.shial@gmail.com>
2020-10-28 00:08:55 +0000

    AssertArraysEqual ver 0.003

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,24 @@
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(actual, expected) {
-  if (actual.length !== expected.length) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  } else {
     console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
-    return;
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
   } else {
     for (let i = 0; i < actual.length; i++) {
       if (actual[i] !== expected[i]) {
-        console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
-        return;
+        return false;
       }
     }
   }
-  console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  return true;
 };
 
 // Test assertion cases
