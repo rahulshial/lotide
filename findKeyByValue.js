8055dc74cf0d805d4541dc976287db60afd57fef
Rahul <rahul.shial@gmail.com>
2020-10-28 22:45:22 +0000

    Find Key by Value ver 0.001

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(searchObject, keySearch) {
+
+  for (const key of Object.keys(searchObject)) {
+    console.log(searchObject[key]);
+    console.log(key);
+    if (searchObject[key] === keySearch) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
