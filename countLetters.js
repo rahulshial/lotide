316eff1e7c272a4a2a705f3c32180664c7d2fc9a
Rahul <rahul.shial@gmail.com>
2020-10-28 21:29:26 +0000

    Count Letters ver 0.002

--- countLetters.js
+++ countLetters.js
@@ -8,29 +8,25 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countLetters = function(string, itemsToCount) {
+const countLetters = function(string) {
   const results = {};
   for (const character of string) {
     if (character !== " ") {
-      if (itemsToCount[character]) {
-        if (results[character]) {
-          results[character] += 1;
-        } else {
-          results[character] = 1;
-        }
+      if (results[character]) {
+        results[character] += 1;
+      } else {
+        results[character] = 1;
       }
     }
   }
-  console.log(results);
   return results;
 };
 
 const stringToCheck = "lighthouse in the house";
-const result1 = countLetters(stringToCheck, { "l": true, "h": false, "t": true, "z": true });
+const result1 = countLetters(stringToCheck);
 
 
 // Test Code
-assertEqual(result1["l"], 1);
-assertEqual(result1["t"], 2);
-assertEqual(result1["z"], undefined);
-assertEqual(result1["h"], undefined);
+assertEqual(result1["h"], 4); // should pass
+assertEqual(result1["e"], 3); // should pass
+assertEqual(result1["l"], 2); // should fail
