c8cafbd2dc6a0344392dd5c8e40e3d5cad164d6f
Rahul <rahul.shial@gmail.com>
2020-11-02 21:08:01 +0000

    refactored to include all modules

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 // index.js
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = ('./without');
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
 };
