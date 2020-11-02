3254fa637c3409424c07ca6ebfedfa6dcb9cc08e
Rahul <rahul.shial@gmail.com>
2020-11-02 17:14:22 +0000

    refactored with Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,20 @@
 // TEST CODE
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    const inputArray = [1, 2, 3];
+    assert.deepEqual(tail(inputArray), [2, 3]);
+  });
 
-const oneWord = ["Yo Yo"];
-tail(oneWord);
-assertEqual(oneWord.length, 1);
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    const inputArray = [5, 6, 7]
+    assert.deepEqual(tail(inputArray), [6, 7]);
+  });
+
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    const inputArray = ["Hello", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(inputArray), ["Lighthouse", "Labs"]);
+  });
+});
