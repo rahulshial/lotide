e78d0e21ac4c3256c9860fdcca64a852aaef0184
Rahul <rahul.shial@gmail.com>
2020-11-02 15:59:58 +0000

    AssertEqual & Head Test code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Apple", "Apple");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), 5);
