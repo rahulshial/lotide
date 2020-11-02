8842ce0030d3290b2392ebf35e63960b09557160
Rahul <rahul.shial@gmail.com>
2020-11-02 17:23:25 +0000

    refactored to include mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,36 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-// TEST CODE
+describe("#middle", () => {
+  it("returns [] for [1] & []", () => {
+    const inputArray1 = [];
+    const inputArray2 = [];
+    const expectedOutput = [];
+    assert.deepEqual(middle(inputArray1, inputArray2), expectedOutput);
+  });
+  it("returns [] for [1, 2] & []", () => {
+    const inputArray1 = [1, 2];
+    const inputArray2 = [];
+    const expectedOutput = [];
+    assert.deepEqual(middle(inputArray1, inputArray2), expectedOutput);
+  });
+  it("returns [2] for [1, 2, 3] & [2]", () => {
+    const inputArray1 = [1, 2, 3];
+    const inputArray2 = [2];
+    const expectedOutput = [2];
+    assert.deepEqual(middle(inputArray1, inputArray2), expectedOutput);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5] & [3]", () => {
+    const inputArray1 = [1, 2, 3, 4, 5];
+    const inputArray2 = [3];
+    const expectedOutput = [3];
+    assert.deepEqual(middle(inputArray1, inputArray2), expectedOutput);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4] & [2, 3]", () => {
+    const inputArray1 = [1, 2, 3, 4];
+    const inputArray2 = [2, 3];
+    const expectedOutput = [2, 3];
+    assert.deepEqual(middle(inputArray1, inputArray2), expectedOutput);
+  });
+});
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual((middle([1, 2])), []); // => []
-assertArraysEqual((middle([1, 2, 3])), [2]); // => [2]
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); // => [3]
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]); // => [2, 3]
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]); // => [3, 4]
