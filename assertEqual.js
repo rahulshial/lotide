900039e5726593e343979a473908d38134122c03
Rahul <rahul.shial@gmail.com>
2020-11-02 15:58:52 +0000

    Refactored to be called as separate functions

--- assertEqual.js
+++ assertEqual.js
@@ -2,14 +2,11 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Apple", "Apple");
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,22 +1,9 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = require('./assertEqual');
 
-  if (actual === expected) {
-    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const head = function(array) {
   return array[0];
 };
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(1, 2);
-// assertEqual("Apple", "Apple");
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), 5);
+module.exports = head;
