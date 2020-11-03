5dd93c8888529303a2a71db6cd31589a82bd9b0c
Rahul <rahul.shial@gmail.com>
2020-11-03 01:10:20 +0000

    refactored to module for package. tested with mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,20 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-// Test assertion cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), true); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), true); // => false
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
+    const array1 = [1, 2, 3];
+    const array2 = [1, 2, 3];
+    assert.deepEqual(eqArrays(array1, array2), true);
+  });
+  it("returns false for [1, 2, 3], [3, 2, 1]", () => {
+    const array1 = [1, 2, 3];
+    const array2 = [3, 2, 1];
+    assert.deepEqual(eqArrays(array1, array2), false);
+  });
+  it("returns true for [[2, 3], [4]], [[2, 3], [4]]", () => {
+    const array1 = [[2, 3], [4]];
+    const array2 = [[2, 3], [4]];
+    assert.deepEqual(eqArrays(array1, array2), true);
+  });
+});
